d3fd36e8bc421afd6e1ca9d547ef5f5e251f8546
Carslake <Toby.Carslake@acer.org>
2020-09-24 14:11:24 +0930

    responsive added toggles for outcomes added

--- js/app.js
+++ js/app.js
@@ -3,52 +3,65 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       const response = JSON.parse(xhttp.responseText);
       const data = response.data;
+
       let allGrid = document.querySelectorAll(".belonging, .mentoring, .social, .cognitive, .behavioural, .physical, .preventing,.primary,.middle,.secondary,.combined,.other,.direct,.indir,.short,.moderate,.longest,.one,.classroom,.small,.medium,.large,.academicrisk,.disadvantaged,.specialneeds,.universal");
       let allGridArray = Array.from(allGrid);
+      let acGrids = document.querySelectorAll(".academic,.numeracy,.literacy,.gpa");
+      let acGridsArray = Array.from(acGrids);
+      let welGrids = document.querySelectorAll(".wellbeing,.socialemotional,.behaviouralskills,.cognitiveskills,.internalising");
+      let welGridsArray = Array.from(welGrids);
 
-        for (i=0; i < allGridArray.length; i++) {
-          let y = allGridArray[i].classList[0];
-          let z = allGridArray[i].classList[1];
-          // console.log(allGridArray[i].classList[0]);
-          // console.log(allGridArray[i].classList[1]);
-          
-          
-          //console.log(data.filter(it => it.intervention.includes(y) && it.outcome.includes(z)));
-          //console.log(pip);
-          let pip = data.filter(it => it.intervention.includes(y) && it.outcome.includes(z));
-          //let growth = pip[0].growth;
-          // console.log(pip[0]);
-        // console.log(allGridArray[i]);
-          
-          //allGrid[i].innerHTML = data.filter(it => it.Intervention.includes(y) && it.Outcomes.includes(z)).length;
-          //&& data.filter(it => it.intervention.includes("academic" || "numeracy" || "literacy" || "gpa")
-          if(pip[0].effect != "n/a")
-          allGrid[i].innerHTML += '<svg class="circle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <circle r=' + pip[0].growth * 5 + ' cx="50%" cy="80%" stroke="FF5F33" stroke-width="2" fill=""/><text x="50%" y="50%" text-anchor="middle" fill="black" font-weight="bold" font-size="2.5em" font-family="" dy=" ">' + '&#43;' + pip[0].growth + '</text></svg>';
-          
+      let acAllColumns = document.querySelectorAll(".academic,.numeracy,.literacy,.gpa,.academicCol,.numeracyCol,.literacyCol,.gpaCol");
+      let welAllColumns = document.querySelectorAll(".wellbeing,.socialemotional,.behaviouralskills,.cognitiveskills,.internalising,.wellbeingCol,.socialemotionalCol,.behaviouralCol,.cognitiveCol,.internalisingCol");
+      
+      for (var a = 0; a < acAllColumns.length; a++) {
+        acAllColumns[a].className += ' ac ';
+      }
+      for (var b = 0; b < welAllColumns.length; b++) {
+        welAllColumns[b].className += ' wel ';
+      }
 
-            //This adds a black circle svg offset as a shadow
-            // allGrid[i].innerHTML += '<svg class="circle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><defs>'+
-            // '<filter id="shadow" x="-20%" y="-20%" width="200%" height="200%">'+
-            // '<feGaussianBlur stdDeviation="2 2" result="shadow"/>'+
-            // '<feOffset dx="6" dy="6"/>'+
-            // '</filter>'+
-            // '</defs> <circle r=' + pip[0].growth * 5 + ' style="filter: url(#shadow); fill: black" cx="50%" cy="80%" stroke="black" stroke-width="2"/><circle r=' + pip[0].growth * 5 + ' cx="50%" cy="80%" stroke="FF5F33" stroke-width="2" fill=""/><text x="50%" y="50%" text-anchor="middle" fill="black" font-weight="bold" font-size="2.5em" font-family="" dy=" ">' + '&#43;' + pip[0].growth + '</text></svg>';
+      //for array of academic outcomes grid to populate hover for academic grids
+      for (k=0; k < acGridsArray.length; k++) {
+        let w = acGridsArray[k].classList[0];
+        let x = acGridsArray[k].classList[1];
+        let acFilter = data.filter(it => it.intervention.includes(w) && it.outcome.includes(x));
+        
+        if(acFilter[0].effect != "n/a")
+        tippy("#" + acGridsArray[k].id, {
+        maxWidth: '',
+        content:  '&#43;' + acFilter[0].growth + ' months learning <br>' + acFilter[0].effect,
+        allowHTML: true,
+        followCursor: 'default',
+        boundary: 'parent',
+        appendTo: document.body
+      });
+      }
 
-
-
-
-          
-          
-            // console.log("done" + i);
-          if(pip[0].effect != "n/a")
-            tippy("#" + allGridArray[i].id, {
+        //for all outcomes g value in grid and svg circle
+        for (j=0; j < welGridsArray.length; j++) {
+          let u = welGridsArray[j].classList[0];
+          let v = welGridsArray[j].classList[1];
+          let welFilter = data.filter(it => it.intervention.includes(u) && it.outcome.includes(v));
+         
+          if(welFilter[0].effect != "n/a")
+            tippy("#" + welGridsArray[j].id, {
             maxWidth: '',
-            content:   pip[0].effect,
+            content:   welFilter[0].effect,
             allowHTML: true,
             followCursor: 'default',
             boundary: 'parent',
             appendTo: document.body
           });
+        };
+
+        //for all outcomes g value in grid and svg circle
+        for (i=0; i < allGridArray.length; i++) {
+          let y = allGridArray[i].classList[0];
+          let z = allGridArray[i].classList[1];
+          let pip = data.filter(it => it.intervention.includes(y) && it.outcome.includes(z));
+          if(pip[0].effect != "n/a")
+            allGrid[i].innerHTML += '<svg class="circle" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"> <circle r=' + pip[0].growth * 5 + ' cx="50%" cy="80%" stroke="FF5F33" stroke-width="2" fill=""/><text x="50%" y="50%" text-anchor="middle" fill="black" font-weight="bold" font-size="2.5em" font-family="" dy=" ">' + pip[0].g + '</text></svg>';
         };
 }};
 xhttp.open("GET", "data.json", true);
@@ -57,67 +70,73 @@ xhttp.send();
 tippy('#setting', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into five groups that aligned with participant’s grade level. These resulting groups were - Primary, Middle, Secondary, Combined K-12 and Other (included Special schools).</p>',
-  interactive: true,
+  
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#mode', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into two modes of implementation, depending on whether students were directly involved in the program, or indirectly (e.g. a teacher development program designed to improve classroom climate, supported by resources for students).</p>',
-  interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#duration', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised as being of short, moderate, or long duration depending on whether they ran for three months or less, up to one year, or more than one year in duration.</p>',
-  interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#grouping', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into groups ranging from individual programs to whole-school activities done through the curriculum at the classroom level. There were also interventions that involved selective groups of students. These groups were defined as small (10 or fewer students), medium (11 to 20 students), and large (more than 20 students).</p>',
-  interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#approach', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised based on their approach into the following categories - Targeted: Academic risk, Targeted: Disadvantaged, Targeted: Special needs and Universal.</p>',
-  interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
-
 //https://www.d3-graph-gallery.com/graph/custom_legend.html#cat1
-var svg = d3.select("#my_dataviz")
-svg.append("circle").attr("cx",25).attr("cy",60).attr("r", 20).style("fill", "#FF5F33")
-// svg.append("circle").attr("cx",20).attr("cy",50).attr("r", 6).style("fill", "#404080")
+var svg = d3.select("#legend")
+svg.append("circle").attr("cx",25).attr("cy",60).attr("r", 15).style("fill", "#FF5F33")
 svg.append("text").attr("x", 5).attr("y", 13).text("Legend").attr("font-weight", "bold").style("font-size", "").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 15).attr("y", 33).text("+2").attr("font-weight", "bold").style("font-size", "1.5em").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 55).attr("y", 33).text(" = number of months of growth compared with control group").style("font-size", "").attr("alignment-baseline","middle")
-svg.append("text").attr("x", 55).attr("y", 50).text("** = significance at < .001; * = significance at < .05").style("font-size", "").attr("alignment-baseline","middle")
-// svg.append("text").attr("x", 50).attr("y", 50).text("variable B").style("font-size", "").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 5).attr("y", 33).text("0.31**").attr("font-weight", "bold").style("font-size", "1.5em").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 65).attr("y", 33).text(" = Hedge’s g").style("font-size", "").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 65).attr("y", 50).text("** = significance at < .001").style("font-size", "").attr("alignment-baseline","middle")
+svg.append("text").attr("x", 65).attr("y", 67).text("* = significance at < .05").style("font-size", "").attr("alignment-baseline","middle")
 
-
-
-// const rows = ["belonging","mentoring","social","behavioural","physical","preventing"];
-// const columns = ["setting","mode","duration","grouping","approach"];
-// let gapGrid = document.querySelectorAll(".belonging, .mentoring, .social, .cognitive, .behavioural, .physical, .preventing,.primary,.middle,.secondary,.combined,.other,.direct,.indirect,.short,.moderate,.long,.one,.classroom,.small,.medium,.large,.academicrisk,.disadvantaged,.specialneeds,.universal")
-// let gapGridArray = Array.from(gapGrid);
-
-// for(h=0; h<gapGrid.length; h++) {
-//     let el = gapGridArray[h]
-//     console.log(gapGridArray[h]);
-
-    
-
-//     for(i=0; i<columns.length; i++) {
-//         el.classList.add(columns[i]);
-//             for (j=0; j<rows.length; j++) {
-//               el.classList.add(rows[j]);
-//             }
-//     }
-// }
-// console.log(gapGridArray)
+function showWel() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(6, 16.666666667vmin)";
+  Array.from(document.querySelectorAll(".wel"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".ac"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+}
+function showAc() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(5, 20vmin)";
+  Array.from(document.querySelectorAll(".wel"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+  Array.from(document.querySelectorAll(".ac"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+} 
+function showAll() {
+  document.getElementById("grid").style.gridTemplateColumns = "100px repeat(9, minmax(min-content, 100px))";
+  Array.from(document.querySelectorAll(".wel"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".ac"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+}
--- js/appgap.js
+++ js/appgap.js
@@ -3,18 +3,48 @@ xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       const response = JSON.parse(xhttp.responseText);
       const dataGap = response.data;
-      console.log(dataGap);
       let gapGrid = document.querySelectorAll(".primary,.middle,.secondary,.combined,.other,.direct,.indir,.short,.moderate,.long,.one,.classroom,.small,.medium,.large,.risk,.disadvantaged,.special,.universal");
       let gapGridArray = Array.from(gapGrid);
+
+
+      let schoolColumns = document.querySelectorAll(".primary,.middle,.secondary,.combined,.other");
+      let modeColumns = document.querySelectorAll(".direct,.indir");
+      let durationColumns = document.querySelectorAll(".short,.moderate,.long");
+      let groupingColumns = document.querySelectorAll(".one,.classroom,.small,.medium,.large");
+      let approachColumns = document.querySelectorAll(".risk,.disadvantaged,.special,.universal");
+      
+      
+      for (var a = 0; a < schoolColumns.length; a++) {
+        schoolColumns[a].className += ' schoolSetting ';
+      }
+      for (var b = 0; b < modeColumns.length; b++) {
+        modeColumns[b].className += ' interventionMode ';
+      }
+      for (var c = 0; c < durationColumns.length; c++) {
+        durationColumns[c].className += ' programDuration ';
+      }
+      for (var d = 0; d < groupingColumns.length; d++) {
+        groupingColumns[d].className += ' groupingSize ';
+      }
+      for (var e = 0; e < approachColumns.length; e++) {
+        approachColumns[e].className += ' interventionApproach ';
+      }
+
+
+
+
+
+
+
+
 
         for (i=0; i < gapGridArray.length; i++) {
           let y = gapGridArray[i].classList[0];
           let z = gapGridArray[i].classList[1];
-          console.log(gapGridArray[i].classList[0]);
-          console.log(gapGridArray[i].classList[1]);
+
 
           let pip = dataGap.filter(it => it.intervention.includes(y) && it.outcome.includes(z));
-          console.log(pip)
+
 
           
           gapGrid[i].innerHTML += '<svg class="circle' + z + '" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%" viewBox="0 0 100 100"> <circle r=' + pip.length * 1.5 + ' cx="50%" cy="50%" stroke="FF5F33" stroke-width="2" fill=""/></svg>';
@@ -51,56 +81,94 @@ xhttp.send();
 tippy('#setting', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into five groups that aligned with participant’s grade level. These resulting groups were - Primary, Middle, Secondary, Combined K-12 and Other (included Special schools).</p>',
-  interactive: true,
+  //interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#mode', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into two modes of implementation, depending on whether students were directly involved in the program, or indirectly (e.g. a teacher development program designed to improve classroom climate, supported by resources for students).</p>',
-  interactive: true,
+  //interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#duration', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised as being of short, moderate, or long duration depending on whether they ran for three months or less, up to one year, or more than one year in duration.</p>',
-  interactive: true,
+  //interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#grouping', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised into groups ranging from individual programs to whole-school activities done through the curriculum at the classroom level. There were also interventions that involved selective groups of students. These groups were defined as small (10 or fewer students), medium (11 to 20 students), and large (more than 20 students).</p>',
-  interactive: true,
+  //interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 tippy('#approach', {
   maxWidth: 500,
   content: '<p style="font-size:110%;">Studies were categorised based on their approach into the following categories - Targeted: Academic risk, Targeted: Disadvantaged, Targeted: Special needs and Universal.</p>',
-  interactive: true,
+  //interactive: true,
   allowHTML: true,
   appendTo: document.body
 });
 
+//.schoolSetting,.interventionMode,.programDuration,.groupingSize,.interventionApproach
 
-// const rows = ["belonging","mentoring","social","behavioural","physical","preventing"];
-// const columns = ["setting","mode","duration","grouping","approach"];
-// let gapGrid = document.querySelectorAll(".belonging, .mentoring, .social, .cognitive, .behavioural, .physical, .preventing,.primary,.middle,.secondary,.combined,.other,.direct,.indirect,.short,.moderate,.long,.one,.classroom,.small,.medium,.large,.academicrisk,.disadvantaged,.specialneeds,.universal")
-// let gapGridArray = Array.from(gapGrid);
-
-// for(h=0; h<gapGrid.length; h++) {
-//     let el = gapGridArray[h]
-//     console.log(gapGridArray[h]);
-
+function setting() {
     
-
-//     for(i=0; i<columns.length; i++) {
-//         el.classList.add(columns[i]);
-//             for (j=0; j<rows.length; j++) {
-//               el.classList.add(rows[j]);
-//             }
-//     }
-// }
-// console.log(gapGridArray)
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(6, 16.67vmin)";
+  Array.from(document.querySelectorAll(".schoolSetting"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".interventionMode,.programDuration,.groupingSize,.interventionApproach"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+}
+function mode() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(3, 33vmin)";
+  Array.from(document.querySelectorAll(".interventionMode"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".schoolSetting,.programDuration,.groupingSize,.interventionApproach"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+} 
+function duration() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(4, 25vmin)";
+  Array.from(document.querySelectorAll(".programDuration"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".schoolSetting,.interventionMode,.groupingSize,.interventionApproach"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+}
+function grouping() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(6, 16.67vmin)";
+  Array.from(document.querySelectorAll(".groupingSize"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".schoolSetting,.interventionMode,.programDuration,.interventionApproach"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+} 
+function approach() {
+  document.getElementById("grid").style.gridTemplateColumns = "repeat(5, 20vmin)";
+  Array.from(document.querySelectorAll(".interventionApproach"))
+  .forEach(function(val) {
+      val.style.display = 'grid';
+  });
+  Array.from(document.querySelectorAll(".schoolSetting,.interventionMode,.programDuration,.groupingSize"))
+  .forEach(function(val) {
+      val.style.display = 'none';
+  });
+}
